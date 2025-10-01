3c17f52d05a6702a4697d9fd1fe39e2bfb961be0
Jaydenz <65591052+JaydenzKoci@users.noreply.github.com>
2025-10-01 00:16:24 -0700

    mmmmmmmmmmmmmm

--- Encore/scripts/copy-ffmpeg-libs.cmake
+++ Encore/scripts/copy-ffmpeg-libs.cmake
@@ -30,7 +30,43 @@ if(FFMPEG_LIB_DIR AND TARGET_DIR)
         math(EXPR COPIED_COUNT "${COPIED_COUNT} + 1")
     endforeach()
     
-    message(STATUS "Copied ${COPIED_COUNT} FFmpeg .so files (all versions)")
+    # Create symlinks for versioned libraries if they don't exist
+    # This ensures that libname.so points to libname.so.X
+    set(FFMPEG_LIBS_TO_LINK
+        "swresample"
+        "avcodec"
+        "avformat"
+        "avutil"
+        "swscale"
+        "avfilter"
+        "avdevice"
+    )
+    
+    foreach(LIB_BASE ${FFMPEG_LIBS_TO_LINK})
+        set(BASE_SO "${TARGET_DIR}/lib${LIB_BASE}.so")
+        
+        # Find the highest versioned library for this base name
+        file(GLOB VERSIONED_LIBS "${TARGET_DIR}/lib${LIB_BASE}.so.*")
+        if(VERSIONED_LIBS)
+            list(GET VERSIONED_LIBS 0 HIGHEST_VERSION)
+            get_filename_component(HIGHEST_VERSION_NAME ${HIGHEST_VERSION} NAME)
+            
+            # Create symlink if base .so doesn't exist or is not a symlink to the right target
+            if(NOT EXISTS ${BASE_SO})
+                message(STATUS "  Creating symlink: lib${LIB_BASE}.so -> ${HIGHEST_VERSION_NAME}")
+                execute_process(
+                    COMMAND ${CMAKE_COMMAND} -E create_symlink ${HIGHEST_VERSION_NAME} lib${LIB_BASE}.so
+                    WORKING_DIRECTORY ${TARGET_DIR}
+                    RESULT_VARIABLE SYMLINK_RESULT
+                )
+                if(NOT SYMLINK_RESULT EQUAL 0)
+                    message(STATUS "  Warning: Failed to create symlink for lib${LIB_BASE}.so")
+                endif()
+            endif()
+        endif()
+    endforeach()
+    
+    message(STATUS "Copied ${COPIED_COUNT} FFmpeg .so files and created necessary symlinks")
 else()
     message(STATUS "FFMPEG_LIB_DIR or TARGET_DIR not set")
 endif()
